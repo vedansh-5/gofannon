bca4278e69edb2c1344ee87a541ff0af2426f0e3
rawkintrevo <trevor.d.grant@gmail.com>
2025-02-22 20:23:02 -0600

    Ship It
    Signed-off-by: rawkintrevo <trevor.d.grant@gmail.com>

--- website/scripts/sync_docs.py
+++ website/scripts/sync_docs.py
@@ -1,14 +1,19 @@
 import os
 import shutil
 from pathlib import Path
+import re
 
 DOCS_SOURCE = Path("docs")
 DOCS_TARGET = Path("website/docs")
-JEKYLL_HEADER = """---  
-layout: docs  
-title: {title}  
----  
+JEKYLL_HEADER = """---    
+layout: docs    
+title: {title}    
+---    
 """
+
+def replace_md_with_html(content):
+    # Regular expression to find markdown links
+    return re.sub(r"(\]\()(.*?)(\.md)(\))", r"\1\2.html\4", content)
 
 def process_md_files(src, dest):
     for item in os.listdir(src):
@@ -21,6 +26,9 @@ def process_md_files(src, dest):
         elif src_path.suffix == ".md":
             with open(src_path, "r") as f:
                 content = f.read()
+
+                # Replace .md references with .html
+            content = replace_md_with_html(content)
 
             title = item[:-3].replace("_", " ").title()
             new_content = JEKYLL_HEADER.format(title=title) + content
