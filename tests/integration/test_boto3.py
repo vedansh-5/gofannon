3f89317337703a5c851e30016181e6d20f0df1f9
jolson-ibm <joe.olson@ibm.com>
2025-04-25 15:35:28 -0500

    Bedrock Framework README.md (#282)
    * Closes #265
    
    Signed-off-by: Joe Olson <joe.olson@ibm.com>
    
    * Closes #265
    
    Signed-off-by: Joe Olson <joe.olson@ibm.com>
    
    * Closes #265
    
    Signed-off-by: Joe Olson <joe.olson@ibm.com>
    
    * closes #265
    
    Signed-off-by: Joe Olson <joe.olson@ibm.com>
    
    * Closes #265
    
    Signed-off-by: Joe Olson <joe.olson@ibm.com>
    
    ---------
    
    Signed-off-by: Joe Olson <joe.olson@ibm.com>

--- tests/integration/test_boto3.py
+++ tests/integration/test_boto3.py
@@ -3,7 +3,7 @@ import json
 import sys
 
 # Pass in as a param
-build = True
+build = True 
 
 if build:
     agent_app_config = {
@@ -28,13 +28,15 @@ if build:
 
     print(json.dumps(bedrock_config, indent=3))
 else:
+    account_id = "<aws account id>"
+    region = "<aws region>"
     bedrock_config = {
-        "lambdaARN": "arn:aws:lambda:us-east-1:843382705282:function:gofannon_demo_iss_locator",
+        "lambdaARN": f"arn:aws:lambda:{region}:{account_id}:function:gofannon_demo_iss_locator",
         "lambdaRoleName": "gofannon_demo_bedrock_lambda_execution_role",
-        "agentId": "HJVJBDR7JJ",
+        "agentId": "I9CGGKG3X8",
         "agentRoleName": "gofannon_demo_bedrock_agent_execution_role",
-        "agentPolicyARN": "arn:aws:iam::843382705282:policy/gofannon_demo_bedrock_agent_allow_model",
-        "agentActionGroup": "ZMQOSJBY7V",
+        "agentPolicyARN": f"arn:aws:iam::{account_id}:policy/gofannon_demo_bedrock_agent_allow_model",
+        "agentActionGroup": "FBWX2MIEZJ",
     }
 
     iss_locator = IssLocator()
